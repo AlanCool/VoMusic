8675db6f6c3c3b8f149cd151b2bcb7f863d48b16
AlanCool <zhuxiaofeng023@163.com>
2019-10-23 20:33:47 +0800

    配置登陆页面 ，增加输入框样式

--- app/src/main/java/com/example/zvt_110/vomusic/activitys/LoginActivity.java
+++ app/src/main/java/com/example/zvt_110/vomusic/activitys/LoginActivity.java
@@ -2,6 +2,7 @@ package com.example.zvt_110.vomusic.activitys;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.zvt_110.vomusic.R;
 
@@ -13,5 +14,8 @@ public class LoginActivity extends BaseActivity {
         setContentView(R.layout.activity_login);
 
         initNavBar(false, "登陆", false);
+    }
+
+    public void onRegistersClick(View view) {
     }
 }
---
+++ app/src/main/java/com/example/zvt_110/vomusic/views/InputView.java
@@ -0,0 +1,79 @@
+package com.example.zvt_110.vomusic.views;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.os.Build;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.annotation.RequiresApi;
+import android.support.annotation.VisibleForTesting;
+import android.text.InputType;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+import com.example.zvt_110.vomusic.R;
+
+
+public class InputView extends FrameLayout {
+
+    private int inputIcon;
+    private String inputHint;
+    private boolean isPassword;
+
+    private View mView;
+    private ImageView mIvIcon;
+    private EditText mEtInput;
+
+
+    public InputView(@NonNull Context context) {
+        super(context);
+        init(context, null);
+    }
+
+    public InputView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init(context, attrs);
+    }
+
+    public InputView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init(context, attrs);
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.LOLLIPOP)
+    public InputView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+        init(context, attrs);
+    }
+
+
+    public void init(Context context, AttributeSet attrs) {
+        if (attrs == null)
+            return;
+
+        TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.inputview);
+        inputIcon = typedArray.getResourceId(R.styleable.inputview_input_icon, R.mipmap.icon1);
+        inputHint = typedArray.getString(R.styleable.inputview_input_hint);
+        isPassword = typedArray.getBoolean(R.styleable.inputview_is_password, false);
+        typedArray.recycle();
+
+        mView = LayoutInflater.from(context).inflate(R.layout.input_view, this, false);
+        mIvIcon = mView.findViewById(R.id.iv_icon);
+        mEtInput = mView.findViewById(R.id.et_input);
+
+        mIvIcon.setImageResource(inputIcon);
+        mEtInput.setHint(inputHint);
+        mEtInput.setInputType(isPassword ? InputType.TYPE_CLASS_TEXT | InputType.TYPE_TEXT_VARIATION_PASSWORD : InputType.TYPE_CLASS_PHONE);
+
+        addView(mView);
+
+    }
+
+    public String getInputStr() {
+        return mEtInput.getText().toString().trim();
+    }
+}
