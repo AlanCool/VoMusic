444d5a4fdc3444a078f9c3ddfa076bc6ed92d412
AlanCool <zhuxiaofeng023@163.com>
2019-10-28 21:43:45 +0800

    增加登录框手机，密码验证模块

--- app/src/main/java/com/example/zvt_110/vomusic/activitys/LoginActivity.java
+++ app/src/main/java/com/example/zvt_110/vomusic/activitys/LoginActivity.java
@@ -1,12 +1,17 @@
 package com.example.zvt_110.vomusic.activitys;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
 import com.example.zvt_110.vomusic.R;
+import com.example.zvt_110.vomusic.utils.UserUtils;
+import com.example.zvt_110.vomusic.views.InputView;
 
 public class LoginActivity extends BaseActivity {
+
+    private InputView mInputpassword,mInputphone;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,8 +19,21 @@ public class LoginActivity extends BaseActivity {
         setContentView(R.layout.activity_login);
 
         initNavBar(false, "登陆", false);
+
+        mInputphone=fd(R.id.input_phone);
+        mInputpassword=fd(R.id.inout_password);
     }
 
     public void onRegistersClick(View view) {
+    }
+
+    public void onCommitClick(View view) {
+        String phone=mInputphone.getInputStr();
+        String password=mInputpassword.getInputStr();
+        if (!UserUtils.validateLogin(this,phone,password)){
+            return;
+        }
+        Intent intent=new Intent(this,MainActivity.class);
+        startActivity(intent);
     }
 }
